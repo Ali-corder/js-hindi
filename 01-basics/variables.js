beff07b1382fc5ce63b997807d6a4421d9c94393
Ali-corder <144464067+Ali-corder@users.noreply.github.com>
2023-09-08 19:41:46 +0000

    yes i want to save this

---
+++ 01-basics/variables.js
@@ -0,0 +1,15 @@
+const nameId = "Ali Raza"
+let accountId = "001254450369856"
+
+let City;
+var stateName = "Bihar"
+/*
+    Prefer not to use var
+    because of issue in block scope and functional scope
+*/
+
+accountPassword = "1583@$#!@"   // This is not good waye
+
+// nameId = 48258  // not allowed
+console.log(nameId);
+console.table([nameId, accountId, stateName, accountPassword, City]);
