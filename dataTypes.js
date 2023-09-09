eb24778eeb521c3164ed4be436e2da6ea12ce73b
Ali-corder <144464067+Ali-corder@users.noreply.github.com>
2023-09-09 08:59:47 +0000

    dataTypes

---
+++ dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"  // Treat all JS Code as newer version
+
+
+// alert(3 + 3)  we are using nodejs, not browser
+
+
+console.log(3
+    +
+    3) //  Code readability should be High
+
+    console.log("Ali Raza")
+
+    let name = "Ali Raza"
+    let age = 25
+    let isLoggedIn = false
+    let state;
+
+    // number => 2 to power 53
+    // bigint
+    // string => "" '' you can use single code or dubale code
+    // boolean => true/false
+    // null => standa lone value
+    // symbole => unique
+
+    // object
+
+    console.log(typeof undefined); // undefind
+    console.log(typeof null);  // object
+
+
